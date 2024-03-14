92b5ae6e41986148ac91450bbe6920985bbc0fdc
Gustavo Almeida <159168821+GuhStvo@users.noreply.github.com>
2024-03-14 17:31:51 -0300

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+'use strict'
+// alert('oi')
+
+
+ var azul = document.getElementById('azul')
+ var verde = document.getElementById('verde')
+ var laranja = document.getElementById('laranja')
+
+ var mensagem = document.getElementById('mensagem')
+
+azul.onclick.document.body.style.backgroundColor = 'blue'
